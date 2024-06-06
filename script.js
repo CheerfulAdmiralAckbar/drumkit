209dcfdbbfe4a5245688ed94ff6f9f3483b3adbb
Ben Johnston <admiralackbar411@gmail.com>
2024-06-06 13:41:04 +0100

    Added in Cymbal and TomDrum through HTML/CSS, added event listener to play audo when each one is clicked

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log(`Hello.`);
+const cymbal = document.getElementById('cymbal');
+const cymbalMain = document.getElementById('cymbal-main'); 
+
+const tomdrum = document.getElementById('tom-drum')
+
+cymbal.addEventListener('click', () => {
+  // starting new audo inside event listener lets auto play multiple times instead of waiting to end
+  const rideSound = new Audio('./sounds/ride.wav');
+  rideSound.play();
+
+  cymbalMain.classList.add('rotated');
+
+  setTimeout(() => {
+    cymbalMain.classList.remove('rotated');
+  }, 200);
+});
+
+tomdrum.addEventListener('click', () => {
+  // starting new audo inside event listener lets auto play multiple times instead of waiting to end
+  const tomSound = new Audio('./sounds/tom.wav');
+  tomSound.play();
+
+});
